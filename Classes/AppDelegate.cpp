1c5a8b160b4169bff597201c0e9a153be6b8b46a
citriccoder <citriccoder@gmail.com>
2016-10-05 19:47:38 +0200

    add GraphicScene

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,5 @@
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+#include "GraphicsScene.h"
 
 USING_NS_CC;
 
@@ -20,7 +20,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
         director->setOpenGLView(glview);
     }
 
-    auto scene = HelloWorld::createScene();
+    auto scene = GraphicsScene::createScene();
     director->runWithScene(scene);
 
     return true;
