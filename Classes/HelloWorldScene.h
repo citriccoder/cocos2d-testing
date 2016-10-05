fc28e924f9cf02a52781b0f4bbb89e9717f211eb
citriccoder <citriccoder@gmail.com>
2016-10-05 16:59:23 +0200

    1rst lesson

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,5 +1,4 @@
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#pragma once
 
 #include "cocos2d.h"
 
@@ -7,14 +6,7 @@ class HelloWorld : public cocos2d::Layer
 {
 public:
     static cocos2d::Scene* createScene();
-
-    virtual bool init();
+    virtual bool init();  
     
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 };
-
-#endif // __HELLOWORLD_SCENE_H__
